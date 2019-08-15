7e82e8825378ad9b7bb87a39b526ad5d021f553a
joannaarroyo <joliuac@gmail.com>
2019-08-15 15:33:37 -0700

    added update test to categories.test.js

--- __tests__/categories.test.js
+++ __tests__/categories.test.js
@@ -31,17 +31,33 @@ describe('Categories Model', () => {
       .catch(e => console.error('ERR', e));
   });
 
+  // TODO: refactor this too so that it doesn't look like callbacks
   it('can get() a category', () => {
     let obj = { name: 'Test Category' };
     return categories.create(obj)
       .then(record => {
         return categories.get(record._id)
+        //TODO: refactor here
           .then(category => {
             Object.keys(obj).forEach(key => {
               expect(category[0][key]).toEqual(obj[key]);
             });
           });
       });
+  });
+
+  it('can update() a category', () => {
+    let obj = { name: 'Test Category' };
+    let obj2 = { name: 'Update Category' };
+    return categories.create(obj)
+      .then(() => categories.create(obj2))
+      .then(record => categories.update(record.id, obj2))
+      .then(category => {
+        Object.keys(obj2).forEach(key => {
+          expect(category[key]).toEqual(obj2[key]);
+        });
+      })
+      .catch(error => console.error('UPDATE ERROR', error));
   });
 
   // TODO: refactor? Put two objects into an array and do forEach to create?
